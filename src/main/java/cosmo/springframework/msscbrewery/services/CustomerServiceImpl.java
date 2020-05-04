fc4cbacc4b058e5b7ffe4eb90b536eb3f4b30cfa
pramodpra <pramod.pradhan@ephesoft.com>
2020-05-04 09:37:24 +0530

    Customer Post PUT and delete end points created

--- src/main/java/cosmo/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/cosmo/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -2,10 +2,12 @@ package cosmo.springframework.msscbrewery.services;
 
 
 import cosmo.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -14,6 +16,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .name("Pramod")
                 .build();
 
+
+    }
+
+    @Override
+    public CustomerDto saveCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID()).build();
+
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //  // todo impl - Update a Customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("deleting Customer....");
 
     }
 }
